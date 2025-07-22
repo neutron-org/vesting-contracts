515b2ea9dc6e9cfc421bd8d440823f8183573e31
pr0n00gler <programmer10110@gmail.com>
2025-07-22 17:03:51 +0300

    original tge packages

---
+++ Makefile
@@ -0,0 +1,25 @@
+.PHONY: schema test clippy build fmt compile check_contracts
+
+schema:
+	@find contracts/* -maxdepth 2 -type f -name Cargo.toml -execdir cargo schema \;
+test:
+	@cargo test
+
+clippy:
+	@cargo clippy --all --all-targets -- -D warnings
+
+fmt:
+	@cargo fmt -- --check
+
+compile:
+	@docker run --rm -v "$(CURDIR)":/code \
+	    --mount type=volume,source="$(notdir $(CURDIR))_cache",target=/target \
+	    --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+	    --platform linux/amd64 \
+	    cosmwasm/workspace-optimizer:0.14.0
+
+check_contracts:
+	@cargo install cosmwasm-check --version 2.0.4 --locked
+	@cosmwasm-check --available-capabilities iterator,staking,stargate,neutron artifacts/*.wasm
+
+build: schema clippy test fmt compile check_contracts
---
+++ packages/astroport/src/oracle.rs
@@ -0,0 +1,79 @@
+use crate::asset::{AssetInfo, PairInfo};
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Addr;
+use cosmwasm_std::{Decimal256, Uint128, Uint256, Uint64};
+
+/// This structure stores general parameters for the contract.
+/// Modified by us
+#[cw_serde]
+pub struct InstantiateMsg {
+    /// The factory contract address
+    pub factory_contract: String,
+    /// Minimal interval between Update{}'s
+    pub period: u64,
+    /// Manager is the only one who can set pair info, if not set already
+    pub manager: String,
+}
+
+/// This structure describes the execute functions available in the contract.
+#[cw_serde]
+pub enum ExecuteMsg {
+    /// Update/accumulate prices
+    Update {},
+    /// Update period
+    UpdatePeriod { new_period: u64 },
+    /// Set a new manager, only owner can use this message
+    UpdateManager { new_manager: String },
+    /// Set asset infos that have a pool for which this contract provides price feeds.
+    /// Only manager can use this message
+    SetAssetInfos(Vec<AssetInfo>),
+}
+
+/// This structure describes the query messages available in the contract.
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+    /// Calculates a new TWAP with updated precision
+    #[returns(Vec<(AssetInfo, Uint256)>)]
+    Consult {
+        /// The asset for which to compute a new TWAP value
+        token: AssetInfo,
+        /// The amount of tokens for which to compute the token price
+        amount: Uint128,
+    },
+    #[returns(Vec<(AssetInfo, Decimal256)>)]
+    TWAPAtHeight {
+        /// The asset for which to compute a new TWAP value
+        token: AssetInfo,
+        /// The amount of tokens for which to compute the token price
+        height: Uint64,
+    },
+    /// Returns the contract's conriguration structure
+    #[returns(Config)]
+    Config {},
+    /// Returns the timestamp of the block when the previous update happened
+    #[returns(u64)]
+    LastUpdateTimestamp {},
+}
+
+/// This structure describes a migration message.
+/// We currently take no arguments for migrations.
+#[cw_serde]
+pub struct MigrateMsg {}
+
+/// Global configuration for the contract
+#[cw_serde]
+pub struct Config {
+    /// The address that's allowed to change contract parameters
+    pub owner: Addr,
+    /// The factory contract address
+    pub factory: Addr,
+    /// The assets in the pool. Each asset is described using a [`AssetInfo`]
+    pub asset_infos: Option<Vec<AssetInfo>>,
+    /// Information about the pair (LP token address, pair type etc)
+    pub pair: Option<PairInfo>,
+    /// Time between two consecutive TWAP updates.
+    pub period: u64,
+    /// Manager is the only one who can set pair info, if not set already
+    pub manager: Addr,
+}
